74061ceabc18bb299b6995569e072e5beb02d371
nahidBdcom <nahid@bdcom.com>
2022-11-14 12:37:42 +0600

    minor modified

--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -20,33 +20,7 @@
 
  @include('partials.review_sub')
 
- {{-- <div class="row align-items-end">
-    <div class="col-lg-6 col-md-6">
-       <a class="btn mt-55" href="#">
-          Post review
-          <i class="fa fa-solid fa-chevron-right btn-icon ml-15"></i>
-       </a>
-    </div>
-    <div class="col-lg-6 col-md-6">
-       <nav aria-label="Page navigation example">
-          <ul class="pagination">
-             <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-                </a>
-             </li>
-             <li class="page-item"><a class="page-link" href="#">1</a></li>
-             <li class="page-item"><a class="page-link" href="#">2</a></li>
-             <li class="page-item"><a class="page-link" href="#">3</a></li>
-             <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-                </a>
-             </li>
-          </ul>
-       </nav>
-    </div>
- </div> --}}
+ 
 
 
 </div>
